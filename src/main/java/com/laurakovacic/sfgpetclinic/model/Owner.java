561038cb6a779035eb59803d8c1f598e2618338e
Laura <laurakovacic@outlook.com>
2023-01-25 21:11:50 +0100

    add POJOs. Closes #2

---
+++ src/main/java/com/laurakovacic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.laurakovacic.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/laurakovacic/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.laurakovacic.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
