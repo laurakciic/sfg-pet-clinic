d81844ad8693f702903d7071a5da06556c57db57
Laura <laurakovacic@outlook.com>
2023-01-26 21:44:36 +0100

    add base entity. Closes #9

--- pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.laurakovacic.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.laurakovacic.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
