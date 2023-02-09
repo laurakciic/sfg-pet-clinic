9284add2e1a4185e7acb951892381ed5e314a537
Laura <laurakovacic@outlook.com>
2023-02-09 20:00:34 +0100

    add Specialty Map Service Impl - closes #30

---
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.laurakovacic.sfgpetclinic.services;
+
+import com.laurakovacic.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
