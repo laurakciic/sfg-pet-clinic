5e20b0f151aa445f7830a0d1b20fcbaaf835da66
Laura <laurakovacic@outlook.com>
2023-02-12 16:16:03 +0100

    implement map impl for visit service. Closes #45

---
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.laurakovacic.sfgpetclinic.services;
+
+import com.laurakovacic.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
--- pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/services/map/AbstractMapService.java
@@ -5,7 +5,9 @@ import com.laurakovacic.sfgpetclinic.model.BaseEntity;
 import java.util.*;
 
 public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
     protected Map<Long, T> map = new HashMap<>();
+
     Set<T> findAll() {
         return new HashSet<>(map.values());
     }
