6c5eeef7bee7baf4e99db39456f13a8d339019cb
Laura <laurakovacic@outlook.com>
2023-02-12 10:04:39 +0100

    update owner with JPA entity mappings - closes #35

--- pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Owner.java
@@ -1,12 +1,23 @@
 package com.laurakovacic.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();    // initializing with default value, so when adding a new pet it will get added automatically no exception will be thrown
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Pet.java
@@ -1,11 +1,24 @@
 package com.laurakovacic.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")      // owner_id property on the pet record
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
