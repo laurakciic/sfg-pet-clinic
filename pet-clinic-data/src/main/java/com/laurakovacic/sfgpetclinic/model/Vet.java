9031e6354f7db306bfc4a208fc3cd66a4ac413a8
Laura <laurakovacic@outlook.com>
2023-02-12 14:26:59 +0100

    update vet with JPA mappings - closes #36

--- pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/laurakovacic/sfgpetclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package com.laurakovacic.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)                    // JPA will try load all at once
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))   // table with IDs from both sides of the relathionship
     private Set<Specialty> specialties = new HashSet<>();   // good practice to initialize with default value so the exception is not thrown when adding values
 
     public Set<Specialty> getSpecialties() {                // this would throw an exception if specialties wasn't initialized
